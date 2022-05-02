e35f1bbfb7bd3289e16982dd4c9bfc123f194c11
afshin-zafari <afshinzafari@gmail.com>
2022-05-02 12:48:19 +0200

    batch v2

--- run_tests.bat
+++ run_tests.bat
@@ -1,3 +1,5 @@
+@echo off
+set MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild"
 set VSTEST="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console"
 set MSTEST_Framework="msTest"
 set XUNIT_Framework="xUnit"
@@ -6,6 +8,26 @@ set msUnitTest=".\WpfApp.%MSTEST_Framework%\bin\Debug\netcoreapp3.1\WpfApp.%MSTE
 set xUnitTest=".\WpfApp.Test.%XUNIT_Framework%\bin\Debug\netcoreapp3.1\WpfApp.Test.%XUNIT_Framework%.dll"
 set nUnitTest=".\WpfApp.Test.%NUNIT_Framework%\bin\Debug\netcoreapp3.1\WpfApp.Test.%NUNIT_Framework%.dll"
 
+
+@echo "===================================================================="
+@echo "                 Build the whole solution"
+@echo "===================================================================="
+%MSBUILD% -t:Rebuild -verbosity:minimal 
+
+@echo ""
+@echo "===================================================================="
+@echo "                 Test msTest unit tests "
+@echo "===================================================================="
 %VSTEST% %msUnitTest%
+
+@echo ""
+@echo "===================================================================="
+@echo "                 Test xUnit unit tests "
+@echo "===================================================================="
 %VSTEST% %xUnitTest%
+
+@echo ""
+@echo "===================================================================="
+@echo "                 Test nUnit unit tests "
+@echo "===================================================================="
 %VSTEST% %nUnitTest%
